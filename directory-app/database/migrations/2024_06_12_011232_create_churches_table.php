46cdafba485f95b433705668d06af1f5a317ba2c
Deibs11 <128659614+deibs11@users.noreply.github.com>
2024-06-11 19:20:17 -0600

    Cambio de version

---
+++ directory-app/database/migrations/2024_06_12_011232_create_churches_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('churches', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('descripcion');
+            $table->string('direccion');
+            $table->string('telefono')->nullable();
+            $table->string('ciudad')->nullable();
+            $table->string('estado');
+            $table->string('pais');
+            $table->string('cp');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('churches');
+    }
+};
